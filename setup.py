542120b2878553fd6e2b1550a027882c1eaf6dcf
Nimrod Flores <nimrodlflores@yahoo.com>
2019-06-09 18:02:09 +0800

    Forked from frappe/bench main repo

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='bench',
+	description='Metadata driven, full-stack web framework',
+	author='Frappe Technologies',
+	author_email='info@frappe.io',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=[
+		'Click',
+		'jinja2',
+		'virtualenv',
+		'requests',
+		'honcho',
+		'semantic_version',
+		'GitPython==0.3.2.RC1'
+	],
+	entry_points='''
+[console_scripts]
+bench=bench.cli:cli
+''',
+)
